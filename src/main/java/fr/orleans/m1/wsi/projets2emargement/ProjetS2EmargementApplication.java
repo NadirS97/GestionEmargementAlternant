2e3314b8ac250331a125347230ce2765452b02ca
Tasdemir Seyma <seyma.tasdemir@etu.univ-orleans.fr>
2022-04-03 11:17:59 +0200

    Initial Commit

---
+++ src/main/java/fr/orleans/m1/wsi/projets2emargement/ProjetS2EmargementApplication.java
@@ -0,0 +1,13 @@
+package fr.orleans.m1.wsi.projets2emargement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetS2EmargementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetS2EmargementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/orleans/m1/wsi/projets2emargement/ProjetS2EmargementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orleans.m1.wsi.projets2emargement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetS2EmargementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
