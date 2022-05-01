2d3a87a0792908169d40ca3e3351f7bc7ed45ceb
SEHOUL Taha <51123425+sehoul@users.noreply.github.com>
2022-05-01 05:04:20 +0200

    Controllers

---
+++ src/main/java/fr/orleans/m1/wsi/projets2emargement/Controller/GroupeController.java
@@ -0,0 +1,86 @@
+package fr.orleans.m1.wsi.projets2emargement.Controller;
+
+import fr.orleans.m1.wsi.projets2emargement.Facade.*;
+import fr.orleans.m1.wsi.projets2emargement.Modele.*;
+import fr.orleans.m1.wsi.projets2emargement.Modele.Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/groupe")
+public class GroupeController {
+
+
+
+    @Autowired
+    private FacadeGroupe facadeGroupe;
+
+    @Autowired
+    private FacadeEtudiant facadeEtudiant;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Groupe>> GetAll(){
+        return ResponseEntity.ok().body(facadeGroupe.findAll());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Groupe> CreateSemestre(@RequestBody Groupe groupe){
+        if(groupe.getNomG()==null || groupe.getNomG().isEmpty())
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }else{
+            if(facadeGroupe.findById(groupe.getNomG()).isPresent())
+            {
+                return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            }else {
+                Groupe g = new Groupe(groupe.getNomG(),new ArrayList<Etudiant>());
+                for (Etudiant e: groupe.getEtudiants()){
+                    if (facadeEtudiant.findById(e.getNumEtu()).isPresent()){
+                        Etudiant et = facadeEtudiant.findById(e.getNumEtu()).get();
+                        et.addGroupe(g.getNomG());
+                        facadeEtudiant.save(et);
+                        g.addEtudiant(et);
+                    }
+                }
+                facadeGroupe.save(g);
+                URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{NomG}")
+                        .buildAndExpand(g.getNomG())
+                        .toUri();
+
+                return ResponseEntity.created(location).body(g);
+            }
+        }
+    }
+
+    @GetMapping("/{NomG}")
+    public ResponseEntity<Groupe> GetSemestreByName(@PathVariable("NomG") String nomG){
+        Optional<Groupe> g=facadeGroupe.findById(nomG);
+        return g.map(value -> ResponseEntity.ok().body(value)).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+//    @DeleteMapping("/{NomG}")
+//    public ResponseEntity<String> DeleteSemestre(@PathVariable("NomG") String NomG) {
+//        Optional<Groupe> g = facadeGroupe.findById(NomG);
+//        if (g.isPresent()) {
+//            for(Optional<Module> m : facadeModule.findBySemestre(nomSemestre)){
+//                m.get().setSemestre("");
+//                facadeModule.save(m.get());
+//            }
+//            facadeSemestre.deleteById(nomSemestre);
+//            return ResponseEntity.ok("Element bien suprime");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Element non trouvable");
+//        }
+//    }
+
+
+}
--- src/main/java/fr/orleans/m1/wsi/projets2emargement/Controller/SousModuleController.java
+++ src/main/java/fr/orleans/m1/wsi/projets2emargement/Controller/SousModuleController.java
@@ -1,4 +1,116 @@
 package fr.orleans.m1.wsi.projets2emargement.Controller;
 
+import fr.orleans.m1.wsi.projets2emargement.Facade.*;
+import fr.orleans.m1.wsi.projets2emargement.Modele.*;
+import fr.orleans.m1.wsi.projets2emargement.Modele.Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/SMod")
 public class SousModuleController {
+
+    @Autowired
+    FacadeSousModule facadeSousModule;
+    @Autowired
+    private FacadeModule facadeModule;
+    @Autowired
+    private FacadeEnseignant facadeEnseignant;
+    @Autowired
+    private FacadeGroupe facadeGroupe;
+
+    @GetMapping("/")
+    public ResponseEntity<List<SousModule>> GetAll(){
+        return ResponseEntity.ok().body(facadeSousModule.findAll());
+    }
+
+    @GetMapping("/{NomSMod}")
+    public ResponseEntity<SousModule> GetModuleByCode(@PathVariable("NomSMod") String nomSMod){
+        Optional<SousModule> Sousmodule=facadeSousModule.findById(nomSMod);
+        return Sousmodule.map(value -> ResponseEntity.ok().body(value)).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<SousModule> CreateModule(@RequestBody SousModule sousModule){
+
+
+        if(     sousModule.getNomSM()==null || sousModule.getNomSM().isEmpty() ||
+                sousModule.getModule()==null || sousModule.getModule().isEmpty() ||
+                sousModule.getTypeCours()==null || sousModule.getTypeCours().toString().isEmpty() ||
+                sousModule.getEnseignant()==null || sousModule.getEnseignant().toString().isEmpty() ||
+                sousModule.getGroupe()==null || sousModule.getGroupe().isEmpty()
+        )
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }else {
+            if (facadeSousModule.findById(sousModule.getNomSM()).isPresent()) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            } else if(facadeModule.findById(sousModule.getModule()).isEmpty()
+                        || facadeEnseignant.findById(sousModule.getEnseignant()).isEmpty()
+                        || facadeGroupe.findById(sousModule.getGroupe()).isEmpty()
+                    ){
+                         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            } else{
+                Module m = facadeModule.findById(sousModule.getModule()).get();
+                Enseignant e = facadeEnseignant.findById(sousModule.getEnseignant()).get();
+                Groupe g = facadeGroupe.findById(sousModule.getGroupe()).get();
+                SousModule sm = new SousModule(sousModule.getNomSM(),m.getNomM(), TypeCours.valueOf(sousModule.getTypeCours().toString()),g.getNomG(),e.getIdEnseignant());
+                m.addSousModule(sm);
+                g.addSousModule(sm);
+                e.addSousModule(sm);
+                facadeModule.save(m);
+                facadeEnseignant.save(e);
+                facadeGroupe.save(g);
+                facadeSousModule.save(sm);
+                URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{NomSMod}")
+                        .buildAndExpand(sm.getNomSM())
+                        .toUri();
+                return ResponseEntity.created(location).body(sm);
+
+            }
+        }
+
+
+    }
+
+//    @DeleteMapping("/{NomSMod}")
+//    public ResponseEntity<String> DeleteModule(@PathVariable("CodeMod") String CodeMod) {
+//        Optional<Module> module = facadeModule.findById(CodeMod);
+//        if (module.isPresent()) {
+//            facadeModule.deleteById(CodeMod);
+//            return ResponseEntity.ok("Element bien suprime");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Element non trouvable");
+//        }
+//    }
+//
+//    @PutMapping("/{NomSMod}")
+//    public ResponseEntity<Module> ModifyModule(@PathVariable("CodeMod") String CodeMod,@RequestBody Module m) {
+//        Optional<Module> module = facadeModule.findById(CodeMod);
+//        if (module.isPresent()) {
+//            if( m.getNomM()==null || m.getNomM().isEmpty() ||
+//                    m.getSemestre()==null || m.getSemestre().isEmpty()){
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//            }else{
+//                module.get().setNomM(m.getNomM());
+//                module.get().setSemestre(m.getSemestre());
+//                facadeModule.save(module.get());
+//                return ResponseEntity.ok(module.get());
+//            }
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//        }
+//    }
+
+
+
+
 }
